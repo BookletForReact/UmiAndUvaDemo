8cf1d8ee035189755869473cab75608ee89e15a0
zhangshaofeng <zhangshaofeng@guazi.com>
2020-01-13 11:51:57 +0800

    feat:umi 配置

--- .umirc.js
+++ .umirc.js
@@ -1,4 +1,3 @@
-
 // ref: https://umijs.org/config/
 export default {
   treeShaking: true,
@@ -7,28 +6,41 @@ export default {
       path: '/',
       component: '../layouts/index',
       routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+        {
+          path: '/register',
+          component: './register/index',
+        },
+        {
+          path: '/login',
+          component: './login/index',
+        },
+        {
+          path: '/',
+          component: '../pages/index',
+        },
+      ],
+    },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: false,
-      dva: true,
-      dynamicImport: false,
-      title: 'github',
-      dll: false,
-      
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
+    [
+      'umi-plugin-react',
+      {
+        antd: false,
+        dva: true,
+        dynamicImport: false,
+        title: '云信demo',
+        dll: false,
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//,
+          ],
+        },
       },
-    }],
+    ],
   ],
-}
+};
