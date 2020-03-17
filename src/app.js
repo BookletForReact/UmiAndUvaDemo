6983287a6935d2ee3367b64a091c0d24828d0ce0
huxiaoxiao1 <huxiaoxiao1@guazi.com>
2020-03-17 13:02:03 +0800

    redux-persist

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
+
 export const dva = {
   config: {
+    onReducer(reducer) {
+      const persistConfig = {
+        key: 'root',
+        storage,
+        stateReconciler: autoMergeLevel2
+      }
+      return persistReducer(persistConfig, reducer)
+    },
     onError(err) {
       err.preventDefault();
       console.error(err.message);
     },
   },
 };
+
+window.onload = () => {
+  persistStore(window.g_app._store)
+}
